0ae419ced2653123c304b65a7a881cae6178eb88
HyeonSoo Choi <chs98105@gmail.com>
2020-11-03 11:49:27 +0000

    Your First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+
+PORT = 4000
+
+function handleListening() {
+    console.log('Listening in 4000 port!')
+}
+
+app.listen(PORT, handleListening)
