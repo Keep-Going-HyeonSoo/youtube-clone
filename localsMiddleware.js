1961bf4d42683cc9b83d952e3a0adbf1800fa71b
HyeonSoo Choi <chs98105@gmail.com>
2020-11-08 06:49:33 +0000

    Template Variables in Pug

--- localsMiddleware.js
+++ localsMiddleware.js
@@ -3,6 +3,5 @@ import routes from './routes'
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = 'HyeonTube'
   res.locals.routes = routes
-  console.log(res)
   next()
 }
