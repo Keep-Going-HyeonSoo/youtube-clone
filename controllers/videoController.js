3b76f24deda3ef30cb92edd0f6c95b9968ea72d6
HyeonSoo Choi <chs98105@gmail.com>
2020-11-08 13:44:52 +0000

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,9 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' })
-export const search = (req, res) => res.render('search', { pageTitle: 'search' })
+export const search = (req, res) => {
+  // console.log(req.query)
+  const { query: { term: searchingBy } } = req
+  res.render('search', { pageTitle: 'search', searchingBy })
+}
 export const upload = (req, res) => res.render('upload', { pageTitle: 'upload' })
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'videoDetail' })
 export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'editVideo' })
