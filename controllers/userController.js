7b1f2ee3bca48bb2a5a0c7501dddc1cee10a6adb
HyeonSoo Choi <chs98105@gmail.com>
2020-12-05 13:48:24 +0000

    etc: todo comment

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,10 @@ export const postJoin = (req, res) => {
   }
 }
 export const getLogin = (req, res) => res.render('login', { pageTitle: 'login' })
-export const postLogin = (req, res) => res.redirect(routes.home)
+export const postLogin = (req, res) => {
+  // to do : log user in
+  res.redirect(routes.home)
+}
 export const logout = (req, res) => res.render('logout', { pageTitle: 'logout' })
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'editProfile' })
 export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'changePassword' })
