4e62916428bc1228fa7df640c99651e6d3216800
HyeonSoo Choi <chs98105@gmail.com>
2021-02-11 13:01:55 +0000

    feat: video Fullscreen and Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,35 +1,50 @@
-const videoContainer = document.getElementById('jsVideoPlayer')
-const videoPlayer = videoContainer.querySelector('video')
+const videoPlayer = document.getElementById('jsVideoPlayer')
+const video = videoPlayer.querySelector('video')
 const playBtn = document.getElementById('jsPlayButton')
 const volumeBtn = document.getElementById('jsVolumeBtn')
+const fullScrnBtn = document.getElementById('jsFullScreen')
 
 function handlePlayClick() {
-  if (videoPlayer.paused) {
-    videoPlayer.play()
+  if (video.paused) {
+    video.play()
     playBtn.innerHTML = '<i class="fas fa-pause"></i>'
   }
   else {
-    videoPlayer.pause()
+    video.pause()
     playBtn.innerHTML = '<i class="fas fa-play"></i>'
   }
 }
 
 function handleVolumeClick() {
-  if (videoPlayer.muted) {
-    videoPlayer.muted = false
+  if (video.muted) {
+    video.muted = false
     volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
   }
   else {
-    videoPlayer.muted = true
+    video.muted = true
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
   }
+}
+
+function goSmallScreen() {
+  document.exitFullscreen()
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>'
+  fullScrnBtn.addEventListener('click', goFullScreen)
+}
+
+function goFullScreen() {
+  videoPlayer.requestFullscreen()
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>'
+  fullScrnBtn.removeEventListener('click', goFullScreen)
+  fullScrnBtn.addEventListener('click', goSmallScreen)
 }
 
 const init = () => {
   playBtn.addEventListener('click', handlePlayClick)
   volumeBtn.addEventListener('click', handleVolumeClick)
+  fullScrnBtn.addEventListener('click', goFullScreen)
 }
 
-if (videoContainer) {
+if (videoPlayer) {
   init()
 }
