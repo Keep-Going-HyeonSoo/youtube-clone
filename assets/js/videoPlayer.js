15ea8fbeb72d61378a05a9fb7c2ba067c94f288d
HyeonSoo Choi <chs98105@gmail.com>
2021-02-11 08:08:18 +0000

    feat: Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,18 +1,33 @@
 const videoContainer = document.getElementById('jsVideoPlayer')
 const videoPlayer = videoContainer.querySelector('video')
 const playBtn = document.getElementById('jsPlayButton')
+const volumeBtn = document.getElementById('jsVolumeBtn')
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play()
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>'
   }
   else {
     videoPlayer.pause()
+    playBtn.innerHTML = '<i class="fas fa-play"></i>'
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
+  }
+  else {
+    videoPlayer.muted = true
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
   }
 }
 
 const init = () => {
   playBtn.addEventListener('click', handlePlayClick)
+  volumeBtn.addEventListener('click', handleVolumeClick)
 }
 
 if (videoContainer) {
